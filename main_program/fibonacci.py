2c3de2b164da356989a7bceb84805aa251388eb9
shaparen <ext-eugene.shaparenko@here.com>
2019-04-12 15:47:50 +0300

    implemented Fibonacci method and added tests

---
+++ main_program/fibonacci.py
@@ -0,0 +1,4 @@
+def generateFibonacci(length):
+    if length == 0: return 0
+    elif length == 1: return 1
+    else: return generateFibonacci(length - 1) + generateFibonacci(length - 2)
---
+++ tests/python_tests.py
@@ -0,0 +1,20 @@
+from main_program.fibonacci import generateFibonacci
+
+
+class TestClass(object):
+
+    def test_fibonachi_1(self):
+        assert generateFibonacci(0) == 0, "Should be 0"
+
+    def test_fibonachi_2(self):
+        assert generateFibonacci(1) == 1, "Should be 1"
+
+    def test_fibonachi_3(self):
+        assert generateFibonacci(2) == 1, "Should be 2"
+
+    def test_fibonachi_4(self):
+        assert generateFibonacci(3) == 2, "Should be 3"
+
+    def test_fibonachi_5(self):
+        assert generateFibonacci(10) == 55, "Should be 55"
+
