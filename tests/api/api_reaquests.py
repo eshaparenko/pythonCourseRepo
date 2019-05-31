edb39e8f0961d1f8210e3816c32dbf2ee0e1f9af
shaparen <ext-eugene.shaparenko@here.com>
2019-05-31 19:03:45 +0300

    tests refactoring

--- tests/api/api_reaquests.py
+++ tests/api/api_reaquests.py
@@ -7,3 +7,9 @@ from tests.api import configuration
 def login(login_info):
     response = requests.post(configuration.PARAMS['base_url'] + configuration.ENDPOINTS['login_ep'], json=login_info)
     return response.status_code
+
+
+def create_issue(issue):
+    response = requests.post(configuration.PARAMS['base_url'] + configuration.ENDPOINTS['issue_ep'], json=issue,
+                             auth=(configuration.PARAMS['login'], configuration.PARAMS['password']))
+    return response.status_code, response.json()
--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -14,3 +14,17 @@ class TestJiraRequests:
     def test_login_positive(self, login_info):
         status_code = login(login_info)
         assert status_code == 200
+
+    @pytest.mark.parametrize('login_info', [
+        {
+            "username": "YevhenShaparenko",
+            "password": "blahblahblah"
+        },
+        {
+            "username": "blahblahblah",
+            "password": "YevhenShaparenko"
+        }])
+    def test_login_negative(self, login_info):
+        status_code, response = login(login_info)
+        assert status_code == 401
+
