bcdea6d8ad11f8cebef949ecc25f9e4262fbd951
shaparen <ext-eugene.shaparenko@here.com>
2019-06-03 14:31:07 +0300

    Added all phases

--- tests/api/api_reaquests.py
+++ tests/api/api_reaquests.py
@@ -6,10 +6,15 @@ from tests.api import configuration
 
 def login(login_info):
     response = requests.post(configuration.PARAMS['base_url'] + configuration.ENDPOINTS['login_ep'], json=login_info)
-    return response.status_code
+    return response
 
 
 def create_issue(issue):
     response = requests.post(configuration.PARAMS['base_url'] + configuration.ENDPOINTS['issue_ep'], json=issue,
                              auth=(configuration.PARAMS['login'], configuration.PARAMS['password']))
-    return response.status_code, response.json()
+    return response
+
+def delete_issue(key):
+    response = requests.delete(configuration.PARAMS['base_url'] + configuration.ENDPOINTS['issue_ep'] + '/' + key,
+                               auth=(configuration.PARAMS['login'], configuration.PARAMS['password']))
+    return response
